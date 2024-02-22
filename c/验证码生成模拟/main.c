5276e63464c30a888a99e6997004e6acbae58c35
ppg42588 <ppg42588@gmail.com>
2024-02-22 22:04:58 +0800

    v1.0

---
+++ c/验证码生成模拟/main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include"main.h"
+
+/* 数字だけの文字列を取る */
+void NumOnly()
+{
+
+}
+
+/* アルファだけの文字列を取る */
+void WordOnly()
+{
+
+}
+
+/* 数字もアルファもある文字列を取る */
+void NumWord()
+{
+
+}
+
+
+
+
+
+
+int main()
+{
+	int* mys = 0U;
+	printf("  ewfiues  ");
+	system("pause");
+	return 0;
+}
---
+++ c/验证码生成模拟/main.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<stdio.h>
+
+/* 数字だけの文字列を取る */
+void NumOnly();
+
+/* アルファだけの文字列を取る */
+void WordOnly();
+
+/* 数字もアルファもある文字列を取る */
+void NumWord();
